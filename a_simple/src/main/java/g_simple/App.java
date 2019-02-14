1bc5cb56cea599da6839c3af35bc8603b20f9f9c
swathi <swathi.pakal@gmail.com>
2019-02-14 17:12:17 +0530

    Maven_Simple_Proj

---
+++ a_simple/src/main/java/g_simple/App.java
@@ -0,0 +1,13 @@
+package g_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
